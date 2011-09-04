8ac56c86ac96fbd7fd4c50eb67214707798a9af1
Ivan Krasin <imkrasin@gmail.com>
2011-09-03 21:46:37 -0700

    Trim ppapi/c/pp_point.h

--- graphics_2d_example.c
+++ graphics_2d_example.c
@@ -15,77 +15,17 @@
 #include "ppapi/c/ppb_core.h"
 //#include "ppapi/c/pp_point.h"
 
-#include "ppapi/c/pp_macros.h"
-#include "ppapi/c/pp_stdint.h"
-
-/**
- * @file
- * This file defines the API to create a 2 dimensional point.
- * 0,0 is the upper-left starting coordinate.
- */
-
-
-/**
- * @addtogroup Structs
- * @{
- */
-/**
- * The PP_Point structure defines the integer x and y coordinates of a point.
- */
 struct PP_Point {
-  /**
-   * This value represents the horizontal coordinate of a point, starting with 0
-   * as the left-most coordinate.
-   */
   int32_t x;
-  /**
-   * This value represents the vertical coordinate of a point, starting with 0
-   * as the top-most coordinate.
-   */
   int32_t y;
 };
 PP_COMPILE_ASSERT_STRUCT_SIZE_IN_BYTES(PP_Point, 8);
 
-/**
- * The PP_FloatPoint structure defines the floating-point x and y coordinates
- * of a point.
- */
 struct PP_FloatPoint {
   float x;
   float y;
 };
 PP_COMPILE_ASSERT_STRUCT_SIZE_IN_BYTES(PP_FloatPoint, 8);
-/**
- * @}
- */
-
-/**
- * @addtogroup Functions
- * @{
- */
-
-/**
- * PP_MakePoint() creates a <code>PP_Point</code> given the x and y coordinates
- * as int32_t values.
- * @param[in] x An int32_t value representing a horizontal coordinate of a
- * point, starting with 0 as the left-most coordinate.
- * @param[in] y An int32_t value representing a vertical coordinate of a point,
- * starting with 0 as the top-most coordinate.
- * @return A <code>PP_Point</code> structure.
- */
-PP_INLINE struct PP_Point PP_MakePoint(int32_t x, int32_t y) {
-  struct PP_Point ret;
-  ret.x = x;
-  ret.y = y;
-  return ret;
-}
-
-PP_INLINE struct PP_FloatPoint PP_MakeFloatPoint(float x, float y) {
-  struct PP_FloatPoint ret;
-  ret.x = x;
-  ret.y = y;
-  return ret;
-}
 
 // #include "ppapi/c/pp_rect.h"
 
