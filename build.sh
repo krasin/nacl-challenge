10017ff59d398a829818e4f315948503e12bd774
Ivan Krasin <imkrasin@gmail.com>
2011-09-03 21:24:09 -0700

    Use x86_64-nacl-gcc instead of g++ (saves 200K)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-x86_64-nacl-g++ -o lala_full.nexe graphics_2d_example.c -lppapi
+x86_64-nacl-gcc -o lala_full.nexe graphics_2d_example.c -lppapi
 cp lala_full.nexe lala.nexe
 x86_64-nacl-strip lala.nexe
