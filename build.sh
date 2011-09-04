9ff6f437649e3fc543c604d594633dc2c72bbe91
Ivan Krasin <imkrasin@gmail.com>
2011-09-04 16:56:49 -0700

    Use -static option that saves some space

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 build_nexe() {
   ARCH=$1 # i686 or x86_64 or arm
-  $ARCH-nacl-gcc -g -o $ARCH-dbg-demo.nexe $ARCH/*.S graphics_2d_example.c -Wl,--defsym,__nacl_rodata_start=0x10000000 -nostdlib -Wl,-T,$ARCH/elf_nacl.x
+  $ARCH-nacl-gcc -static -g -o $ARCH-dbg-demo.nexe $ARCH/*.S graphics_2d_example.c -Wl,--defsym,__nacl_rodata_start=0x10000000 -nostdlib -Wl,-T,$ARCH/elf_nacl.x
   cp $ARCH-dbg-demo.nexe $ARCH-opt-demo.nexe
   $ARCH-nacl-strip $ARCH-opt-demo.nexe  
 }
