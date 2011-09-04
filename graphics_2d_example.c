3fbb66948e1fe90ebaea982983321594a223ff1a
Ivan Krasin <imkrasin@gmail.com>
2011-09-03 22:13:48 -0700

    Inline ppapi/c/pp_size.h

--- graphics_2d_example.c
+++ graphics_2d_example.c
@@ -9,7 +9,54 @@
 #include "ppapi/c/pp_errors.h"
 #include "ppapi/c/pp_instance.h"
 #include "ppapi/c/pp_module.h"
-#include "ppapi/c/pp_size.h"
+//#include "ppapi/c/pp_size.h"
+#include "ppapi/c/pp_macros.h"
+#include "ppapi/c/pp_stdint.h"
+
+/**
+ * @file
+ * This file defines the width and height of a 2D rectangle.
+ */
+
+
+/**
+ * @addtogroup Structs
+ * @{
+ */
+/**
+ * The <code>PP_Size</code> struct contains the size of a 2D rectangle.
+ */
+struct PP_Size {
+  /** This value represents the width of the rectangle. */
+  int32_t width;
+  /** This value represents the height of the rectangle. */
+  int32_t height;
+};
+PP_COMPILE_ASSERT_STRUCT_SIZE_IN_BYTES(PP_Size, 8);
+/**
+ * @}
+ */
+
+/**
+ * @addtogroup Functions
+ * @{
+ */
+
+/**
+ * PP_MakeSize() creates a <code>PP_Size</code> given a width and height as
+ * int32_t values.
+ *
+ * @param[in] w An int32_t value representing a width.
+ * @param[in] h An int32_t value representing a height.
+ *
+ * @return A <code>PP_Size</code> structure.
+ */
+PP_INLINE struct PP_Size PP_MakeSize(int32_t w, int32_t h) {
+  struct PP_Size ret;
+  ret.width = w;
+  ret.height = h;
+  return ret;
+}
 
 //#include "ppapi/c/pp_bool.h"
 
