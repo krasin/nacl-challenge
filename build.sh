f517d2fdbbcf0eb482009624e425bc0fb51aba2c
Ivan Krasin <imkrasin@gmail.com>
2011-09-04 16:01:35 -0700

    Rename lala.nexe -> x86_64-opt-demo.nexe, lala_full.nexe -> x86_64-dbg-demo.nexe

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-x86_64-nacl-gcc -g -o lala_full.nexe x86_64/*.S graphics_2d_example.c -Wl,--defsym,__nacl_rodata_start=0x10000000 -nostdlib -Wl,-T,x86_64/elf_nacl.x #-lnacl #-lc
-cp lala_full.nexe lala.nexe
-x86_64-nacl-strip lala.nexe
+x86_64-nacl-gcc -g -o x86_64-dbg-demo.nexe x86_64/*.S graphics_2d_example.c -Wl,--defsym,__nacl_rodata_start=0x10000000 -nostdlib -Wl,-T,x86_64/elf_nacl.x #-lnacl #-lc
+cp x86_64-dbg-demo.nexe x86_64-opt-demo.nexe
+x86_64-nacl-strip x86_64-opt-demo.nexe
