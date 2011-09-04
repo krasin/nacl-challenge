336604d6a6ee0378be25da9df25edc23c73cea92
Ivan Krasin <imkrasin@gmail.com>
2011-09-03 22:16:25 -0700

    Inline ppapi/c/pp_module.h

--- graphics_2d_example.c
+++ graphics_2d_example.c
@@ -8,7 +8,31 @@
 #include "ppapi/c/pp_completion_callback.h"
 #include "ppapi/c/pp_errors.h"
 #include "ppapi/c/pp_instance.h"
-#include "ppapi/c/pp_module.h"
+
+// #include "ppapi/c/pp_module.h"
+#include "ppapi/c/pp_macros.h"
+#include "ppapi/c/pp_stdint.h"
+
+/**
+ * @file
+ * This file defines the PP_Module type which uniquely identifies the module
+ * or .nexe.
+ */
+
+
+/**
+ * @addtogroup Typedefs
+ * @{
+ */
+/**
+ * The PP_Module value uniquely identifies the module or .nexe.
+ *
+ * This identifier is an opaque handle assigned by the browser to the module. It
+ * is guaranteed never to be 0, so a module can initialize it to 0 to
+ * indicate a "NULL handle."
+ */
+typedef int32_t PP_Module;
+PP_COMPILE_ASSERT_SIZE_IN_BYTES(PP_Module, 4);
 
 // #include "ppapi/c/pp_size.h"
 struct PP_Size {
