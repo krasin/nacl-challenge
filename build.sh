68c0dc7de3e3cf87e507925f7a1273432ab2e9ea
Ivan Krasin <imkrasin@gmail.com>
2011-09-03 21:11:27 -0700

    build.sh: keep unstripped binary

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-x86_64-nacl-g++ -o lala.nexe graphics_2d_example.c -lppapi
+set -e
+x86_64-nacl-g++ -o lala_full.nexe graphics_2d_example.c -lppapi
+cp lala_full.nexe lala.nexe
 x86_64-nacl-strip lala.nexe
