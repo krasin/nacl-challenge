b49626d550849b67a37570b4c76c2ff508c5913e
Ivan Krasin <imkrasin@gmail.com>
2011-09-04 15:53:55 -0700

    Move x86_64 assembly files into x86_64 directory

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-x86_64-nacl-gcc -g -o lala_full.nexe startup/*.S graphics_2d_example.c -Wl,--defsym,__nacl_rodata_start=0x10000000 -nostdlib -Wl,-T,elf64_nacl.x #-lnacl #-lc
+x86_64-nacl-gcc -g -o lala_full.nexe x86_64/*.S graphics_2d_example.c -Wl,--defsym,__nacl_rodata_start=0x10000000 -nostdlib -Wl,-T,elf64_nacl.x #-lnacl #-lc
 cp lala_full.nexe lala.nexe
 x86_64-nacl-strip lala.nexe
